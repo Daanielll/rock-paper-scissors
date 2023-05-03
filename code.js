0847c28a9daeda95d442dad880a7f37086fbae6d
Daniel Moses <dm98912@gmail.com>
2023-05-03 22:04:42 +0300

    Finished pt.two

--- code.js
+++ code.js
@@ -30,7 +30,55 @@ function play(playerSelection, computerSelection) {
   } else score = "error";
   return score;
 }
+/*
 for (let i = 0; i < 5; i++) {
   let p = prompt("Enter Pick");
   console.log(play(p, getComputerChoice()));
-}
+} 
+*/
+
+const btns = document.querySelectorAll(".btns > button");
+const results = document.querySelector(".results");
+const plscr = document.querySelector(".plscr");
+const cmpscr = document.querySelector(".cmpscr");
+const reset = document.querySelector(".reset");
+const winn = document.querySelector(".winner");
+let s = [0, 0];
+btns.forEach((b) => {
+  b.addEventListener("click", function (e) {
+    let p = e.target.classList[0];
+    let r = play(p, getComputerChoice());
+    //updates scores
+    if (r === "player") s[0]++;
+    else if (r === "computer") s[1]++;
+
+    //prints round winner
+    results.textContent = r;
+
+    //prints scores
+    plscr.textContent = `Player: ${s[0]}`;
+    cmpscr.textContent = `Computer: ${s[1]}`;
+
+    //stops after reaching five
+    if (s[0] > 4 || s[1] > 4) {
+      btns.forEach((bs) => {
+        bs.disabled = true;
+      });
+      if (s[0] === 5) winn.textContent = "YOU WON!";
+      else winn.textContent = "YOU LOST!";
+      reset.classList.remove("opac");
+    }
+  });
+  reset.addEventListener("click", function (e) {
+    s[0] = 0;
+    s[1] = 0;
+    btns.forEach((bs) => {
+      bs.disabled = false;
+    });
+    plscr.textContent = `Player: 0`;
+    cmpscr.textContent = `Computer: 0`;
+    reset.classList.add("opac");
+    winn.textContent = "";
+    results.textContent = "";
+  });
+});
